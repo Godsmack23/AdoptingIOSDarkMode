0672ee664c135d5cf08be6eae9a9d9fe544a7280
Apple <>
2019-05-31 07:45:01 -0800

    Initial commit.

---
+++ AdoptingDarkMode/AppDelegate.swift
@@ -0,0 +1,16 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The application delegate. Nothing interesting to see here.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
